4e73577b7fd7744b605a1dd8b93fdbcef247b2da
TWAHIRWA Dieudonné <twahirwa25@gmail.com>
2021-12-09 12:00:47 +0100

    create strapi project for backend

---
+++ backend/config/database.js
@@ -0,0 +1,18 @@
+module.exports = ({ env }) => ({
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'bookshelf',
+      settings: {
+        client: 'mysql',
+        host: env('DATABASE_HOST', 'host'),
+        port: env.int('DATABASE_PORT', 'port'),
+        database: env('DATABASE_NAME', 'database'),
+        username: env('DATABASE_USERNAME', 'username'),
+        password: env('DATABASE_PASSWORD', 'password'),
+        ssl: env.bool('DATABASE_SSL', false),
+      },
+      options: {}
+    },
+  },
+});
---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '39157da1c0b1bf52b81240f91b18393d'),
+    },
+  },
+});

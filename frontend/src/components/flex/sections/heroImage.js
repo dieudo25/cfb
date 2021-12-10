76864dee2f1dc21ba11d26bbe470fc2e7ec70d72
TWAHIRWA Dieudonné <twahirwa25@gmail.com>
2021-12-10 14:39:36 +0100

    add style to heroImage component

--- frontend/src/components/flex/sections/heroImage.js
+++ frontend/src/components/flex/sections/heroImage.js
@@ -3,6 +3,16 @@ import ReactMarkdown from "react-markdown";
 
 import generateStrapiUrl from '../../../utils/strapi';
 
+const sectionStyle = {
+    width: "100vw",
+    height: "100vh",
+}
+
+const ImgStyle = {
+    width: "100%",
+    height: "100%",
+}
+
 const HeroImage = ({ data:
     {strapi_component,
     text,
@@ -12,8 +22,15 @@ const HeroImage = ({ data:
         css_classes,
     }}
 }) => (
-    <section id={css_id ? css_id : ''} className={`component ${strapi_component} ${css_classes}`} >
-        <img src={generateStrapiUrl(image.url)} alt={image.alternativeText} />
+    <section 
+        id={css_id ? css_id : ''}
+        className={`component ${strapi_component} ${css_classes}`}
+        style={{sectionStyle}}
+    >
+        <img 
+            src={generateStrapiUrl(image.url)}
+            alt={image.alternativeText}
+        />
         <ReactMarkdown>{text}</ReactMarkdown>
     </section>
 )
--- frontend/src/components/page.js
+++ frontend/src/components/page.js
@@ -2,12 +2,8 @@ import React from 'react';
 import Layout from './layout';
 import DynamicZone from './flex/dynamicZone';
 
-const Page = ({ id, slug, title, full_width }) => (
+const Page = ({ full_width }) => (
     <Layout>
-      {console.log('full_width', full_width)}
-        <h2>{ title }</h2>
-        <p>{ id }</p>
-        <p>{ slug }</p>
         <DynamicZone sections={full_width}/>
     </Layout>
 );

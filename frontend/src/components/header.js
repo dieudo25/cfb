1b4d528e650e32470c661f756adf4d03631af0d7
TWAHIRWA Dieudonné <twahirwa25@gmail.com>
2021-12-11 14:27:35 +0100

    delete style

--- frontend/src/components/header.js
+++ frontend/src/components/header.js
@@ -3,23 +3,19 @@ import PropTypes from "prop-types"
 import { Link } from "gatsby"
 
 const header = {
-  background: `rebeccapurple`,
-  marginBottom: `1.45rem`,
+  
 }
 
 const container = {
-  margin: `0 auto`,
-  maxWidth: 960,
-  padding: `1.45rem 1.0875rem`,
+  
 }
 
 const h1 = {
-  margin: 0
+  
 }
 
 const link = {
-  color: `white`,
-  textDecoration: `none`,
+  
 }
 
 const Header = ({ siteTitle }) => (

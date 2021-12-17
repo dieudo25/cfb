c58afe5a92f8295c59445ec7be5f4e235213696c
TWAHIRWA Dieudonné <twahirwa25@gmail.com>
2021-12-17 09:12:20 +0100

    update slider component + add slick style

--- frontend/src/components/flex/sections/sliderCard.js
+++ frontend/src/components/flex/sections/sliderCard.js
@@ -1,19 +1,20 @@
 import React from "react";
 import tw, { styled } from "twin.macro"
 import Slider  from "react-slick";
+import "../../../style/slick.css"
 
 import Image from "../../basic/image";
 
 
 
-const SSection1 = styled.section`
+const SSection = styled.section`
     ${ tw`
         mx-auto w-10/12 min-h-[calc(80vh - 40px)]
     ` }
 
     div.card-slider {
         ${ tw`
-        
+            grid grid-cols-3 grid-rows-2        
         ` }
 
         .card-container {
@@ -43,65 +44,34 @@ const SSection1 = styled.section`
 
 `
 
-const SSection = styled.section`
-    ${ tw`
-        mx-auto w-10/12 min-h-[calc(80vh - 40px)]
-    ` }
-
-    .card-slider {
-
-        .card-container {
-            ${ tw`
-                w-[225px]
-            ` }
-
-            .img-container {
-                ${ tw`
-                    mx-auto w-[225px] h-[350px]
-                ` }
-
-                img {
-                    ${ tw`
-                        w-full h-full object-cover
-                    ` }
-                }
-            }
-        }
-    }
-
-    
-
-`
-
-
 const SliderCard = ({
     data: {
         strapi_component,
         card,
         style,
         settings,
-        responsive_settings
+        responsive_settings,
     } 
 }) => {
 
     const slideSettings = {
         className: "card-slider",
-        autoplay: false,
+        autoplay: settings.auto_play,
         speed: settings.speed,
         arrows: settings.arrows,
         dots: settings.dots,
         fade: settings.fade,
-        infinite: false,
+        infinite: settings.infinite,
         pauseOnFocus: settings.pause_on_focus,
         pauseOnHover: settings.pause_on_hover,
         pauseOnDotsHover: settings.pause_on_dots_hover,
-        slidesToShow: 3,
+        slidesToShow: settings.slide_to_show,
         slideToScroll: settings.slide_to_scroll,
         swipeToSlide: settings.swipe,
         vertical: settings.direction === "vertical" ? true : false,
         verticalSwiping: settings.vertical_swipe,
         centerMode: settings.center_mode,
-        centerPadding: settings.center_padding
+        centerPadding: settings.center_padding,
     }
 
     return (

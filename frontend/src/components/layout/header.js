eb63e2a22f0f23ceee0d042ab9e1796f46bea2ab
TWAHIRWA Dieudonné <twahirwa25@gmail.com>
2021-12-18 14:47:20 +0100

    Add border to header for visualization

--- frontend/src/components/layout/header.js
+++ frontend/src/components/layout/header.js
@@ -5,8 +5,13 @@ import tw, { styled } from "twin.macro"
 
 const SHeader = styled.header`
   ${ tw`
-    fixed z-10 top-0 left-0 right-0 bg-white h-20
+    fixed z-10 top-0 left-0 right-0 bg-white h-20 border-b-2 border-red-500 border-solid
   ` }
+
+  div {
+    ${ tw`
+    ` }
+  }
 `
 
 const Header = ({ siteTitle }) => (

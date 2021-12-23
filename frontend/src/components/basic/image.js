158f602d37ee3e51d1fc7e4320c8ce5fac977f2c
Kiza-coder <kiza.motoko@gmail.com>
2021-12-23 16:44:40 +0100

    Update file : richText2Image.js

--- frontend/src/components/basic/image.js
+++ frontend/src/components/basic/image.js
@@ -2,9 +2,9 @@ import React from "react";
 
 import generateStrapiUrl from "../../utils/strapi";
 
-const Image = ({ image: { url, alternativeText }, dataSal, dataSalDuration }) => (
+const Image = ({ image: { url, alternativeText }, dataSal, dataSalDuration, className }) => (
     <div 
-        className="img-container"
+        className={ "img-container " + className} 
         data-sal={ dataSal }
         data-sal-duration={ dataSalDuration }
     >
--- frontend/src/components/flex/sections/richText2Image.js
+++ frontend/src/components/flex/sections/richText2Image.js
@@ -5,6 +5,55 @@ import RichText from "../../basic/richText";
 import Image from "../../basic/image";
 
 const SSection = styled.section`
+    ${ tw`
+        grid w-10/12 mx-auto my-[100px] gap-5 mb-0  
+    `}
+
+    div.img-container.image-1 {
+
+        ${ tw`
+        ` }
+
+        ${ ({ className }) => className.includes('about-cfb') && tw`
+            md:object-cover
+        ` }
+
+        img {
+            ${ tw`
+
+            ` }
+
+            ${ ({ className }) => className.includes('about-cfb') && tw`
+                object-contain w-full
+            ` }
+        }
+
+    }
+
+    div.img-container.image-2 {
+
+        ${ tw`
+            
+        ` }
+
+        ${ ({ className }) => className.includes('about-cfb') && tw`
+            w-[100px] mx-auto
+            
+        ` }
+
+        img {
+            ${ tw`
+
+            ` }
+
+            ${ ({ className }) => className.includes('about-cfb') && tw`
+                object-contain w-full 
+            ` }
+        }
+
+    }
+
+
     
 `
 
@@ -22,24 +71,27 @@ const RichText2Image = ({
             animation_image_2 
         } 
 }) => (
+    
     <SSection
         id={ style && style.css_id }
         className={ `component ${strapi_component} ${style && style.css_classes }` }
         first_element= { first_element }
         second_element= { second_element }
     >
-        {console.log("image1", image_1)}
-        {console.log("image2", image_2)}
+        {console.log("firselement:" , second_element    )}
+        {console.log("secondelemen:", first_element)}
         <RichText text={ text }/>
         <Image 
             image={image_1}
-            dataSal={ animation_image_1 }
+            dataSal={ animation_image_1 && animation_image_1.type.replace('_', '-') }
             dataSalDuration="800"
+            className = 'image-1'
         />
         <Image 
             image={image_2}
-            dataSal={ animation_image_2 }
+            dataSal={ animation_image_2 && animation_image_2.type.replace('_', '-') }
             dataSalDuration="800"
+            className= 'image-2'
         />
 
         

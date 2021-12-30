47bd77ec78c801a6397c8b8de7892b0232aa9140
TWAHIRWA Dieudonné <twahirwa25@gmail.com>
2021-12-30 14:19:12 +0100

    up

--- backend/script/deploy.sh
+++ backend/script/deploy.sh
@@ -4,10 +4,10 @@
 /opt/plesk/node/14/bin/npm install yarn --scripts-prepend-node-path
 
 # Install dependecies using yarn
-/opt/plesk/node/14/bin/yarn install --scripts-prepend-node-path
+yarn install --scripts-prepend-node-path
 
 # Build project
-NODE_ENV=production /opt/plesk/node/14/bin/yarn build --scripts-prepend-node-path
+NODE_ENV=production yarn build --scripts-prepend-node-path
 
 # Restart node app
 touch ./tmp/restart.txt
